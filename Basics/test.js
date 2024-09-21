cb5f0a0ed2f8015ba8a2426aed4a7a8dd594fa77
Ankit Singh Senwal <85737230+AnkitSinghSenwal@users.noreply.github.com>
2024-09-21 14:20:58 +0000

    variables in javascript

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,29 @@
-console.log("Hello world...")
+// const accountId = 3232;
+
+// const personName = "TNT";
+// const dob = "12/12/1901";
+// let age = 22n;
+// age = 'twenty two'
+// let test;
+
+// console.log("Hello world...")
+// console.table([personName, dob, age, accountId, test])
+
+// console.log(typeof age)
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+// console.log(null == 0 )
+// console.log(null > 0 )
+// console.log(null >= 0 )
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+  
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id == anotherId)
+console.table([id, anotherId])
